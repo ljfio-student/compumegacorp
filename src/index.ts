cbbba6b168a8c0fa2324acdc2c8d46bcf045d561
Luke Fisher <mail@lukefisher.me>
2018-04-17 11:35:18 +0100

    Organise imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import bodyParser from "body-parser";
 import express from "express";
 import http from "http";
-import bodyParser from "body-parser";
 import { format } from "util";
 import api from "./api";
 
