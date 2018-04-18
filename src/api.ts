480fb0db28843bc516f0cc1cde4b083c4aba51a8
Luke Fisher <mail@lukefisher.me>
2018-04-18 11:13:40 +0100

    Add in passport

--- src/api.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-let api = express.Router();
-
-api.get('/user', (req: express.Request, res: express.Response) => {
-    res.send('hello world').end();
-});
-
-export default api;
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import bodyParser from "body-parser";
 import express from "express";
 import http from "http";
 import { format } from "util";
-import api from "./api";
+import passport from "passport";
+import { Strategy as BearerStrategy } from "passport-http-bearer";
 
+// Setup Express (Static / Body Parser)
 let port = process.env.PORT || 8080;
 
 let app = express();
@@ -12,12 +14,22 @@ let server = http.createServer(app);
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
+// Passport authentication
+passport.use(new BearerStrategy((token: string, done: (error: any, user: any) => void) => {
+
+}));
+
+// Main index.html page
 app.get("/", (req: express.Request, res: express.Response) => {
     res.sendFile("./public/index.html");
 });
 
+// Setup the API routes
+let api = express.Router();
+
 app.use('/api/', api);
 
+// Start the NodeJS server
 server.listen(port, () => {
     console.log(format("Listening on port %s", port));
 });
