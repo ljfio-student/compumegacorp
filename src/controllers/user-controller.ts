eede356f44fbb1fe4058bc7db9e5c29e51c07e56
Luke Fisher <mail@lukefisher.me>
2018-04-18 12:11:25 +0100

    Add in API method to get user information

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,18 +1,47 @@
 import express from "express";
 import { PassportStatic } from "passport";
-import { Db } from "mongodb";
+import { Db, Collection } from "mongodb";
+import { IUser, ISimpleUser } from "../models/user";
 
 export class UserController {
+    private collection: Collection;
+
     constructor(
         private router: express.Router,
         private db: Db,
         private passport: PassportStatic)  {
 
+        this.collection = db.collection('user');
+
         // Setup routes
-        router.get('/user', this.getUser.bind(this));
+        router.get('/user/:id?', passport.authenticate('bearer', {session: false}), this.getUser.bind(this));
     }
 
     private getUser(req: express.Request, res: express.Response) {
-        res.send('hello world').end();
+        // Check that we are logged in
+        if (req.user == null) {
+            return res.status(401).end();
+        }
+
+        // Get the user parameter
+        let id = req.param('id');
+
+        if (id == null) {
+            let user = req.user as ISimpleUser;
+            res.send(user).end();
+        } else {
+            this.collection.findOne({_id: id})
+                .then((user: ISimpleUser) => {
+                    if (user) {
+                        res.send(user).end();
+                    } else {
+                        res.status(404).end();
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).end();
+                });
+        }
     }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,13 @@
+import { ObjectId } from "mongodb";
+
 export interface IUser {
+    _id: ObjectId;
     name: string;
     email: string;
     password: string;
+}
+
+export interface ISimpleUser {
+    _id: ObjectId;
+    name: string;
 }
