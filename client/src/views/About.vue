8679e1a0ad3b66c81881acac4763fa7d2a4adac1
Luke Fisher <mail@lukefisher.me>
2018-05-01 21:36:44 +0100

    Add about helper

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,5 +2,54 @@
   <div class="about container">
     <h1>CompuMegaCorp</h1>
     <p>The ultimate work simulator&hellip;</p>
+    <p>
+      Choose tasks to work on.
+      One of them goes wrong.
+      You can blame other people or take the hit yourself.
+
+      You lose merit if you are the person taking the blame.
+      If you take it and own up to it, you gain honesty but lose merit.
+
+      If you’re found out to have been responsible and haven’t taken the blame, the merit goes down further.
+    </p>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Merit</th>
+          <th scope="col">Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>100</td>
+          <td>New Start</td>
+        </tr>
+        <tr>
+          <td>200</td>
+          <td>Supervisor</td>
+        </tr>
+        <tr>
+          <td>300</td>
+          <td>Assistant Manager</td>
+        </tr>
+        <tr>
+          <td>400</td>
+          <td>Manager</td>
+        </tr>
+        <tr>
+          <td>500</td>
+          <td>Vice President</td>
+        </tr>
+        <tr>
+          <td>600</td>
+          <td>Senior Vice President</td>
+        </tr>
+        <tr>
+          <td>700</td>
+          <td>CEO</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
